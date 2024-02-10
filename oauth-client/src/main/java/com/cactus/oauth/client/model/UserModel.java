deb2985a93365d8ff157435708e6efa86120bf2d
Neel Priyadarshi <neel0108p@gmail.com>
2024-02-09 21:37:19 -0700

    Refactoring (#15)
    * clean up
    
    * clean up

--- oauth-client/src/main/java/com/cactus/oauth/client/model/UserModel.java
+++ oauth-client/src/main/java/com/cactus/oauth/client/model/UserModel.java
@@ -12,24 +12,24 @@ import lombok.NoArgsConstructor;
 public class UserModel {
 
 	@NotBlank(message = "First Name Can Not Be Blank!")
-	@NotNull(message = "First Name Can Not Be Empty!")
 	private String firstName;
 
+	@NotBlank(message = "Last Name Can Not Be Blank!")
 	private String lastName;
 
 	@NotBlank(message = "Email Address Can Not Be Blank!")
-	@NotNull(message = "Email Address Can Not Be Empty!")
 	@Email(regexp = "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,3}", flags = Pattern.Flag.CASE_INSENSITIVE)
 	private String email;
 
 	@NotBlank(message = "Password Can Not Be Blank!")
-	@NotNull(message = "Password Can Not Be Empty!")
+	@Size(min = 8, max = 30)
+	@Pattern(message = "Password Not Meeting Minimum Requirements",
+			regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,30}$")
 	private String password;
 
 	private String matchingPassword;
 
 	@NotBlank(message = "Role Can Not Be Blank!")
-	@NotNull(message = "Role Can Not Be Empty!")
 	@ValidateEnum(acceptedValues = UserRole.class, message = "Invalid User Role")
 	private String role;
 
